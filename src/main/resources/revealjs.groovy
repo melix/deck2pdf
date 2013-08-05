eb294b1d21caab583bc27598ed08426534ea9348
Cedric Champeau <cedric.champeau@gmail.com>
2013-08-05 10:42:22 +0200

    Add 'skipFragments' option to revealjs profile. Fixes #3.

--- src/main/resources/revealjs.groovy
+++ src/main/resources/revealjs.groovy
@@ -9,4 +9,8 @@ nextSlide = {
 setup = {
     // disable controls for better rendering
     js 'Reveal.configure({controls: false, progress: false});'
+
+    if (Boolean.valueOf(options.skipFragments)) {
+        js 'Reveal.configure({fragments: false});'
+    }
 }
